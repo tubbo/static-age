48b3b672c9e44c270098f700947ca114a07a8392
Tom Scott <tscott@telvue.com>
2015-01-29 23:50:26 -0500

    Bug fixes since $ isnt defined in an addon-ed app

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
-import $ from 'jquery';
 
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('article', this._resolveArticleID(params));
   },
 
-  setupController: function(controller, article) {
-    $.get('/articles/'+article.get('id')+'.html', function(html) {
-      article.set('body', new Ember.Handlebars.SafeString(html));
-    });
-    this._super(controller, article);
+  afterModel: function(article, params) {
+    var articleID = article.get('id');
+    if (articleID) {
+      $.get('/articles/'+articleID+'.html', function(html) {
+        article.set('body', new Ember.Handlebars.SafeString(html));
+      });
+    }
   },
 
   _resolveArticleID: function(params) {
